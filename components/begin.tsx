335df2ba330584fa954d20f95e4bb99adfe7bfcb
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-11-25 21:43:47 +0530

    code changed

--- components/begin.tsx
+++ components/begin.tsx
@@ -8,23 +8,22 @@ function begin() {
   };
   return (
     <>
-      <div className=" flex md:flex-row flex-col justify-between items-center my-16 xl:border-y 
-      xl:border-gray-900 text-[#dcdcdc]">
-        <div className="md:w-2/3">
+      <div className=" flex md:flex-row flex-col justify-between items-center m-16 text-[#dcdcdc]">
+        <div className="md:w-1/3">
           <img
-            src="/code.png"
+            src="/code.svg"
             alt="React Code snippet"
             className="w-full h-full opacity-80"
           />
         </div>
         <div className="w-full md:w-1/3 flex flex-col items-center justify-center  m-8 text-center">
-          <h3 className="mx-16 my-4 md:mx-0">
+          <h3 className="mx-8 my-4 md:mx-0">
             Ready to turn your code into a symphony of innovation?
           </h3>
-          <h3 className="mx-16 my-4 md:mx-0">Join us on this adventure!</h3>
+          <h4 className="mx-8 my-4 md:mx-0">Join us on this adventure!</h4>
           <button
             onClick={handleClick}
-            className="font-mono mx-16 md:mx-0 w-2/3 md:w-full border-gray-500"
+            className="font-mono mx-8 md:mx-0 w-2/3 md:w-full border-gray-500"
           >
             Let's Go
           </button>
