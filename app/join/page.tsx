db39f334d83c5c4895e12c71be8d0ee8b6e19090
Manas Malla <manasmalla.dev@gmail.com>
2024-07-07 01:39:46 +0530

    made changes to form page
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -73,35 +73,40 @@ const MyForm: React.FC = () => {
           className="mb-4 px-12 py-4 rounded-md w-full lg:w-4/5 flex flex-col"
         >
           {/* Name */}
-          <label htmlFor="name" className="text-3xl">
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            required
-            placeholder="Type your name..."
-            value={formState.name}
-            onChange={handleChange}
-          />
-
-          {/* Email */}
-          <label htmlFor="email" className="text-3xl">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            required
-            placeholder="Type your email ID..."
-            value={formState.email}
-            onChange={handleChange}
-          />
+          <div className="w-full flex gap-x-8">
+            <div className="flex flex-col grow">
+              <label htmlFor="name" className="text-2xl">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                required
+                placeholder="Type your name..."
+                value={formState.name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex flex-col grow">
+              {/* Email */}
+              <label htmlFor="email" className="text-2xl">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                required
+                placeholder="Type your email ID..."
+                value={formState.email}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
 
           {/* Interested */}
-          <label htmlFor="interested" className="text-3xl">
+          <label htmlFor="interested" className="text-2xl">
             Why are you interested? (Include your past experiences)
           </label>
           <textarea
@@ -116,54 +121,64 @@ const MyForm: React.FC = () => {
             cols={50}
             placeholder="Tell us why you are interested in joining us..."
           ></textarea>
-
-          {/* GitHub Link */}
-          <label htmlFor="githubLink" className="text-3xl">
-            GitHub Profile
-          </label>
-          <input
-            type="url"
-            id="githubLink"
-            name="githubLink"
-            required
-            value={formState.githubLink}
-            placeholder="https://github.com/"
-            onChange={handleChange}
-          />
-
-          {/* Domain */}
-          <label className="text-3xl">Domain</label>
-          <div className="flex justify-between items-center md:flex-row flex-col ">
-            {["App", "Web", "Cloud", "DevOps", "DSA", "ML", "Blockchain"].map(
-              (domain) => (
-                <div
-                  key={domain}
-                  className={`domain ${
-                    formState.domain === domain
-                      ? "bg-black text-white"
-                      : "bg-white text-black"
-                  }`}
-                  onClick={() => handleDomainChange(domain)}
-                >
-                  <input
-                    type="radio"
-                    id={domain}
-                    name="domain"
-                    value={domain}
-                    checked={formState.domain === domain}
-                    onChange={() => ({})}
-                    className="ml-6"
-                  />
-                  <label htmlFor={domain} className="mr-6">
-                    {domain}
-                  </label>
-                </div>
-              )
-            )}
+          <div className="w-full flex gap-x-8">
+            <div className="flex flex-col grow">
+              {/* GitHub Link */}
+              <label htmlFor="githubLink" className="text-2xl">
+                GitHub Profile
+              </label>
+              <input
+                type="url"
+                id="githubLink"
+                name="githubLink"
+                required
+                value={formState.githubLink}
+                placeholder="https://github.com/"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex flex-col grow">
+              {/* Domain */}
+              <label className="text-2xl">Domain</label>
+              <div className="flex justify-between items-center md:flex-row flex-col ">
+                {[
+                  "App",
+                  "Web",
+                  "Cloud",
+                  "DevOps",
+                  // "DSA",
+                  "ML",
+                  // "Blockchain",
+                ].map((domain) => (
+                  <div
+                    key={domain}
+                    className={`domain ${
+                      formState.domain === domain
+                        ? "bg-black text-white"
+                        : "bg-white text-black"
+                    }`}
+                    onClick={() => handleDomainChange(domain)}
+                  >
+                    <input
+                      type="radio"
+                      id={domain}
+                      name="domain"
+                      value={domain}
+                      checked={formState.domain === domain}
+                      onChange={() => ({})}
+                      className="ml-6"
+                    />
+                    <label htmlFor={domain} className="m-4">
+                      {domain}
+                    </label>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
           <button
             onClick={handleSaveData}
-            className="hover:bg-teal-200 bg-black border-teal-700"
+            className="hover:bg-[#874CCC] hover:text-white bg-black w-max mx-auto px-12 py-5 text-xl rounded-full"
           >
             Submit
           </button>
